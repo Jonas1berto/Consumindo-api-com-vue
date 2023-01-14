b6bbc47f21d720a12d1ce1ed3f41a5b5ca181020
Jonas Humberto <jonas.99858439@gmail.com>
2023-01-14 13:55:35 -0300

    Consumindo api com vue

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container grid-lg my-2 py-2 ">
+    <div class="card">
+
+      <div class="card-header p-centered">
+        <h4 class="h4">Moedas convertidas.</h4>
+      </div>
+
+      <div class="card-body">
+        <TableCoins :moedas="moedas" />
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import TableCoins from '@/components/TableCoins.vue'
+import api from '@/serve/api.js'
+
+export default {
+  name: 'App',
+  components: {
+    TableCoins
+  },
+  data() {
+    return {
+      moedas: []
+    }
+  },
+  created() {
+    this.chamarApi()
+  },
+  methods: {
+    async chamarApi() {
+      const response = await api.all() 
+      this.moedas = response.data
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+
+body {
+  background: radial-gradient(rgb(16, 13, 13), rgb(64, 65, 66)) !important;
+}
+
+.container {
+  background: #333;
+}
+
+.card {
+  background: linear-gradient(#333, rgb(8, 8, 8)) !important;
+  color: #fff;
+  box-shadow: rgb(218, 216, 216) 0px 3px 8px;
+}
+
+tr:hover {
+  transition: all ease 0.6s;
+  background: #4e4b4b;
+}
+</style>
---
+++ src/components/TableCoins.vue
@@ -0,0 +1,48 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Códigos</th>
+        <th>Nome</th>
+        <th>Máximo</th>
+        <th>Mínimo</th>
+        <th>Variação</th>
+        <th></th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(moeda, index) in moedas " :key="index">
+        <td> {{ moeda.code }} </td>
+        <td> {{ moeda.name }}</td>
+        <td> {{ moeda.high }}</td>
+        <td> {{ moeda.low }}</td>
+        <td> 
+          <span class="label label-rounded" :class=" {
+          'label-error': moeda.pctChange < 0,
+          'label-success': moeda.pctChange > 0,
+          'label-warning': moeda.pctChange === 0
+        }">
+        {{ moeda.pctChange }} %
+        </span> 
+       </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TableCoins',
+  props: {
+    moedas: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
